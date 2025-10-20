17e4efd5804bb0d9b6abcd92790ef89c91e160e7
renato <renatocrescenti@gmail.com>
2025-10-19 23:55:57 -0300

    DbHandler: add function to populate table when empty and helper functions
    Configuration file: change .env to config.json for user and system info

--- src/main/java/org/jb/invoicereader/DbHandler.java
+++ src/main/java/org/jb/invoicereader/DbHandler.java
@@ -1,13 +1,13 @@
 package org.jb.invoicereader;
 
+import tools.jackson.core.JsonParser;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.net.http.HttpResponse;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public enum DbHandler {
     INSTANCE;
@@ -32,7 +32,8 @@ public enum DbHandler {
                     "ctpCod",
                     "descricao",
                     "prjCod");
-        } catch (ClassNotFoundException | SQLException e) {
+            populatePessoasTable();
+        } catch (ClassNotFoundException | SQLException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
 
@@ -66,7 +67,161 @@ public enum DbHandler {
         stmt.execute(String.valueOf(sql));
     }
 
-    public String getInfoPessoa() {
+    private void populatePessoasTable() throws IOException, InterruptedException, SQLException {
+        ConexosAPI conexos = ConexosAPI.getInstance();
+        System.out.println("populating db");
+        boolean finished = false;
+        int pageNum = 1;
+        while (finished == false) {
+            finished = true;
+            System.out.println("Page num: " + pageNum);
+            HttpResponse<String> response = conexos.PostRequest("cmn025/list", "{\n" +
+                    "\"fieldList\":[\n" +
+                    "    \"pesCod\",\n" +
+                    "    \"dpeCodSeq\",\n" +
+                    "    \"dpeNomPessoa\",\n" +
+                    "    \"dpeNomFantasia\",\n" +
+                    "    \"dpeDtaValidade\",\n" +
+                    "    \"vldValido\",\n" +
+                    "    \"pesVldStatus\"\n" +
+                    "],\n" +
+                    "\"filterList\":{\n" +
+                    "    \"vldValido#EQ\":\"1\"\n" +
+                    "},\n" +
+                    "\"pageNumber\":" + pageNum++ + ",\n" +
+                    "\"pageSize\":1000,\n" +
+                    "\"serviceName\":\"cmn025\",\n" +
+                    "\"orderList\":{\n" +
+                    "    \"orderList\":[\n" +
+                    "        {\n" +
+                    "        \"propertyName\":\"pesCod\",\n" +
+                    "        \"order\":\"asc\"\n" +
+                    "        }\n" +
+                    "    ]\n" +
+                    "}\n" +
+                    "}");
+            if (response.statusCode() != 200) return;
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode json = mapper.readTree(response.body());
+            JsonNode rows = json.get("rows");
+            for (JsonNode row : rows) {
+                System.out.println(row);
+                String pesCod = String.valueOf(row.get("pesCod"));
+                if (checkIfPessoaExists(pesCod)) continue;
+                finished = false;
+                String endCod = getEndCod(pesCod);
+                String docFederal = getDocFederal(pesCod);
+                String dtaValidade = String.valueOf(row.get("dpeDtaValidade"));
+                String descricao = String.valueOf(row.get("dpeNomFantasia"));
+                if (pesCod.isEmpty() || endCod == null || docFederal == null
+                    || dtaValidade.isEmpty() || descricao.isEmpty()) continue;
+                setPessoaRow(pesCod, descricao, endCod, docFederal, dtaValidade);
+            }
+        }
+    }
+
+    private String getEndCod(String pesCod) throws IOException, InterruptedException {
+        ConexosAPI conexos = ConexosAPI.getInstance();
+
+        HttpResponse<String> response = conexos.PostRequest("cmn025/endPessoas/list", "{\n" +
+                "\"filterList\":{\n" +
+                "    \"pesCod#EQ\":\"" + pesCod + "\"\n" +
+                "},\n" +
+                "\"pageNumber\":1,\n" +
+                "\"pageSize\":50,\n" +
+                "\"orderList\":{\n" +
+                "    \"orderList\":[\n" +
+                "        {\n" +
+                "        \"propertyName\":\"endCod\",\n" +
+                "        \"order\":\"desc\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}\n" +
+                "}");
+        if (response.statusCode() != 200) return null;
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(response.body());
+        if (Integer.parseInt(String.valueOf(json.get("count"))) == 0) return "0";
+        String endCod = String.valueOf(json.get("rows").get(0).get("endCod"));
+        return endCod;
+    }
+
+    private String getDocFederal(String pesCod) throws IOException, InterruptedException {
+        ConexosAPI conexos = ConexosAPI.getInstance();
+
+        HttpResponse<String> response = conexos.PostRequest("cmn025/endPessoas/list", "{\n" +
+                "    \"fieldList\":[\n" +
+                "        \"pesCod\",\n" +
+                "        \"pdcDocFederal\",\n" +
+                "        \"pdcDocFederalFmt\"\n" +
+                "    ],\n" +
+                "    \"filterList\":{\n" +
+                "        \"pesCod#EQ\":\"" + pesCod + "\"\n" +
+                "    },\n" +
+                "    \"pageNumber\":1,\n" +
+                "    \"pageSize\":50,\n" +
+                "    \"serviceName\":\"cmn025\",\n" +
+                "    \"orderList\":{\n" +
+                "        \"orderList\":[\n" +
+                "            {\n" +
+                "            \"propertyName\":\"pesCod\",\n" +
+                "            \"order\":\"desc\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    }\n" +
+                "}");
+        if (response.statusCode() != 200) return null;
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(response.body());
+        if (Integer.parseInt(String.valueOf(json.get("count"))) == 0) return "N/A";
+        String docFederal = String.valueOf(json.get("rows").get(0).get("pdcDocFederal")).replaceAll("\"", "");
+        return docFederal;
+    }
+
+    private void updatePessoasTable(String key, String column, String data) throws SQLException {
+        String sql = "UPDATE pessoas SET ? = ? WHERE pesCod = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, column);
+        pstmt.setString(2, data);
+        pstmt.setString(3, key);
+        pstmt.executeUpdate();
+    }
+
+    private void updateDespesasTable() {
+
+    }
+
+    public String getPesCod(String cnpj) throws SQLException {
+        String sql = "SELECT pesCod from pessoas WHERE docFederal = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, cnpj);
+        ResultSet rs = pstmt.executeQuery();
+        if (rs.next()) {
+            return rs.getString("pesCod");
+        }
         return null;
+    }
+
+    public String[] getDespesaData(String code) {
+        return null;
+    }
+
+    public void setPessoaRow(String key, String... data) throws SQLException {
+        String sql = "INSERT OR IGNORE INTO pessoas (pesCod, descricao, endCod, docFederal, dtaValidade) VALUES (?,?,?,?,?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, key);
+        pstmt.setString(2, data[0]);
+        pstmt.setString(3, data[1]);
+        pstmt.setString(4, data[2]);
+        pstmt.setString(5, data[3]);
+        pstmt.executeUpdate();
+    }
+
+    private boolean checkIfPessoaExists(String pesCod) throws SQLException {
+        String sql = "SELECT pesCod FROM pessoas WHERE pesCod = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, pesCod);
+        ResultSet rs = pstmt.executeQuery();
+        return rs.next();
     }
 }
