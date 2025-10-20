25e4845e777b79cb4b7de1fc9c0834367390ca92
renato <renatocrescenti@gmail.com>
2025-10-19 23:52:03 -0300

    DataProcessor: add basic field checking methods, WIP

--- src/main/java/org/jb/invoicereader/DataHandlers/DataProcessor.java
+++ src/main/java/org/jb/invoicereader/DataHandlers/DataProcessor.java
@@ -1,29 +1,94 @@
 package org.jb.invoicereader.DataHandlers;
 
-import tools.jackson.core.*;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.databind.node.ObjectNode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class DataProcessor {
     private final ObjectNode data;
 
     public DataProcessor(JsonNode extracted_data) {
         System.out.println(extracted_data);
+//        System.out.println(extracted_data.get("CEDENTE").get(0));
         data = JsonMapper.shared().createObjectNode();
-        data.put("CEDENTE","MULTILOG BRASIL S.A.")
-            .put("CNPJ","60.526.977/0019-06")
-            .put("NUM_DOCUMENTO","128159")
-            .put("VENCIMENTO","13/08/2025")
-            .put("EMISSAO","29/07/2025")
-            .put("PAGADOR","CENTRAIS ELETRICAS BRASILEIRAS SA")
-            .put("VALOR_TOTAL","2060,74")
-            .put("COD_PESSOA_CEDENTE", "7473")
-            .put("COD_PESSOA_PAGADOR", "8000")
-            .putNull("CNPJ_PAGADOR");
+//        data.put("CEDENTE","MULTILOG BRASIL S.A.")
+//            .put("CNPJ","60.526.977/0019-06")
+//            .put("NUM_DOCUMENTO","128159")
+//            .put("VENCIMENTO","13/08/2025")
+//            .put("EMISSAO","29/07/2025")
+//            .put("PAGADOR","CENTRAIS ELETRICAS BRASILEIRAS SA")
+//            .put("VALOR_TOTAL","2060,74")
+//            .put("COD_PESSOA_CEDENTE", "7473")
+//            .put("COD_PESSOA_PAGADOR", "8000")
+//            .putNull("CNPJ_PAGADOR");
+        checkValorTotal(extracted_data.get("VALOR_TOTAL"));
+        data.put("CEDENTE",checkField(extracted_data.get("CEDENTE")))
+                .put("CNPJ",checkField(extracted_data.get("CNPJ")))
+                .put("NUM_DOCUMENTO",checkField(extracted_data.get("NUM_DOCUMENTO")))
+                .put("VENCIMENTO",checkField(extracted_data.get("VENCIMENTO")))
+                .put("EMISSAO",checkField(extracted_data.get("EMISSAO")))
+                .put("PAGADOR",checkField(extracted_data.get("PAGADOR")))
+                .put("VALOR_TOTAL",checkValorTotal(extracted_data.get("VALOR_TOTAL")))
+                .put("COD_PESSOA_CEDENTE", "7473")
+                .put("COD_PESSOA_PAGADOR", "8000")
+                .putNull("CNPJ_PAGADOR");
     }
 
     public ObjectNode getData() {
         return data;
+    }
+
+    private String checkField(JsonNode data) {
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> dataList = mapper.convertValue(data, new TypeReference<>() {});
+        if (dataList.size() == 1 || dataList.stream().allMatch(element -> Objects.equals(dataList.getFirst(), element))) {
+            System.out.println("Equal");
+            return dataList.getFirst();
+        }
+
+        return null;
+    }
+
+    private String checkValorTotal(JsonNode data) {
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> dataList = mapper.convertValue(data, new TypeReference<>() {});
+        String value = null;
+        System.out.println("here");
+        if (dataList.size() == 1 || dataList.stream().allMatch(element -> Objects.equals(dataList.getFirst(), element))) {
+            value = dataList.getFirst();
+        } else {
+            System.out.println("here2");
+            Map<String, Integer> entryCount = new HashMap<>();
+            for (int i = 0; i < dataList.size() - 1; i++) {
+                for (int j = i + 1; j < dataList.size(); j++) {
+                    if (Objects.equals(dataList.get(i), dataList.get(j))) {
+                        if (entryCount.containsKey(dataList.get(i))) {
+                            entryCount.put(dataList.get(i), entryCount.get(dataList.get(i)) + 1);
+                        } else {
+                            entryCount.put(dataList.get(i), 1);
+                        }
+                    }
+                }
+            }
+            value = entryCount.entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(null);
+        }
+        System.out.println("Value: " + value);
+        String formattedDefault = value.replaceAll(",", "")
+                                        .replaceAll(" ", "")
+                                        .replaceAll("\\.", "");
+        formattedDefault = new StringBuilder(formattedDefault).insert(formattedDefault.length() - 2, ",").toString();
+        return formattedDefault;
+    }
+
+    private String fetchCodPessoa(String cnpj) {
+
+        return null;
     }
 }
