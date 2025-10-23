7f193f0666df6faba6cdf2b755a80af904d5529a
rscres <renatocrescenti@gmail.com>
2025-10-23 17:42:49 -0300

    CreateExpense: implement setEmptyDates for set 'EMISSAO' and 'VENCIMENTO' to current date if fields not filled. GenerateSP, add missing required field('filCod'), fix doument number field name('espNumero') and 'docDtaEntrada' date getting methods

--- src/main/java/org/jb/invoicereader/CreateExpense.java
+++ src/main/java/org/jb/invoicereader/CreateExpense.java
@@ -1,22 +1,31 @@
 package org.jb.invoicereader;
 
+import org.jb.invoicereader.Database.DbHandler;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.node.ObjectNode;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
-import java.time.Instant;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class CreateExpense {
-    private ConexosAPI conexosApi = ConexosAPI.getInstance();
+    private final ConexosAPI conexosApi = ConexosAPI.getInstance();
 
-    public CreateExpense(ObjectNode data) throws IOException, InterruptedException {
+    public CreateExpense(ObjectNode data) throws Exception {
+        DbHandler db = DbHandler.getInstance();
+        String endCod = db.getEndCod(data.get("COD_PESSOA_CEDENTE").asString());
         System.out.println(data);
-//        String pgsCodSeq = addSP(data);
+        if (endCod == null) throw new RuntimeException("Não foi possivel achar o endereço do cedente");
+        data.put("endCod", endCod);
+
+        checkMandatoryFields(data);
+        setEmptyDates(data);
+
+        System.out.println("here2");
         System.out.println("{\n" +
                 "    \"priCod\": " + data.get("priCod").asString() + ",\n" +
                 "    \"prjCod\": " + data.get("prjCod").asString() + ",\n" +
@@ -28,51 +37,60 @@ public class CreateExpense {
                 "    \"pgsFltTaxa\": 1,\n" +
                 "    \"pgsDtaVcto\": " + convertDate(data.get("VENCIMENTO").asString()) + "\n" +
                 "}");
+
+//        String pgsCodSeq = addSP(data);
+
         String pgsCodSeq = "3";
         System.out.println("{\n" +
             "    \"priCod\": " + data.get("priCod").asString() + ",\n" +
-                    "    \"docVldTipo\": 9,\n" +
-                    "    \"gcdCod\": 80,\n" +
-                    "    \"frontModelName\": \"gerConfiguraDoc\",\n" +
-                    "    \"pesCod\": " + data.get("COD_PESSOA_CEDENTE") + ",\n" +
-                    "    \"endCod\": " + data.get("endCod") + ",\n" +
-                    "    \"gcdDesNome\": \"DESPESAS RECIBO IMPORTAÇÃO - PRÓPRIO\",\n" +
-                    "    \"docDtaEntrada\": " + LocalDate.now().atStartOfDay(ZoneId.of("America/Sao_Paulo")).toInstant().toEpochMilli() + ",\n" +
-                    "    \"dtaEmissao\": " + convertDate(data.get("EMISSAO").asString()) + ",\n" +
-                    "    \"dtaValidade\": " + convertDate(data.get("VENCIMENTO").asString()) + ",\n" +
-                    "    \"pdcDocFederal\": \"" + data.get("CNPJ").asString().replaceAll("[^a-zA-Z0-9]", "") + "\",\n" +
-                    "    \"items\": [\n" +
-                    "        {\n" +
-                    "            \"pgsCodSeq\": " + pgsCodSeq + "\n" +
-                    "        }\n" +
-                    "    ]\n" +
-                    "}");
+            "    \"docVldTipo\": 9,\n" +
+            "    \"gcdCod\": 80,\n" +
+            "    \"espNumero\": \"" + data.get("NUM_DOCUMENTO").asString() + "\",\n" +
+            "    \"frontModelName\": \"gerConfiguraDoc\",\n" +
+            "    \"pesCod\": " + data.get("COD_PESSOA_CEDENTE") + ",\n" +
+            "    \"endCod\": " + data.get("endCod") + ",\n" +
+            "    \"gcdDesNome\": \"DESPESAS RECIBO IMPORTAÇÃO - PRÓPRIO\",\n" +
+            "    \"docDtaEntrada\": " + LocalDate.now().atStartOfDay(ZoneId.of("GMT")).toInstant().toEpochMilli() + ",\n" +
+            "    \"dtaEmissao\": " + convertDate(data.get("EMISSAO").asString()) + ",\n" +
+            "    \"dtaValidade\": " + convertDate(data.get("VENCIMENTO").asString()) + ",\n" +
+            "    \"pdcDocFederal\": \"" + data.get("CNPJ").asString().replaceAll("[^a-zA-Z0-9]", "") + "\",\n" +
+            "    \"items\": [\n" +
+            "        {\n" +
+            "            \"pgsCodSeq\": " + pgsCodSeq + "\n" +
+            "        }\n" +
+            "    ]\n" +
+            "}");
+
+//        generateSP(pgsCodSeq, data);
     }
 
-    private void checkMandatoryFields(ObjectNode data) {
-        try {
-            data.get("VALOR_TOTAL").asString();
-            data.get("priCod").asString();
-            data.get("COD_PESSOA_CEDENTE");
-            data.get("endCod");
-            data.get("CNPJ").asString();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    private void setEmptyDates(ObjectNode data) {
+        LocalDate date = LocalDate.ofEpochDay(LocalDate.now().atStartOfDay(ZoneId.of("GMT")).toInstant().toEpochMilli());
+        if (data.get("VENCIMENTO") == null) data.put("VENCIMENTO", String.valueOf(date));
+        if (data.get("EMISSAO") == null) data.put("EMISSAO", String.valueOf(date));
+    }
+
+    private void checkMandatoryFields(ObjectNode data) throws Exception {
+        if (data.get("VALOR_TOTAL") == null || data.get("priCod") == null ||
+            data.get("COD_PESSOA_CEDENTE") == null || data.get("endCod") == null ||
+            data.get("CNPJ") == null)
+            throw new IOException("Campos obrigatórios não preenchidos");
     }
 
     private String addSP(ObjectNode data) throws IOException, InterruptedException {
+        String valor = data.get("VALOR_TOTAL").asString().replace(",", ".");
         HttpResponse<String> response = conexosApi.PostRequest("imp021/processoSp", "{\n" +
                 "    \"priCod\": " + data.get("priCod").asString() + ",\n" +
                 "    \"prjCod\": " + data.get("prjCod").asString() + ",\n" +
                 "    \"ctpCod\": " + data.get("ctpCod").asString() + ",\n" +
                 "    \"vldStatus\": 2,\n" +
-                "    \"pgsMnyMneg\": " + data.get("VALOR_TOTAL").asString() + ",\n" +
-                "    \"pgsMnyMnac\": " + data.get("VALOR_TOTAL").asString() + ",\n" +
+                "    \"pgsMnyMneg\": " + valor + ",\n" +
+                "    \"pgsMnyMnac\": " + valor + ",\n" +
                 "    \"moeCod\": 790,\n" +
                 "    \"pgsFltTaxa\": 1,\n" +
                 "    \"pgsDtaVcto\": " + convertDate(data.get("VENCIMENTO").asString()) + "\n" +
                 "}");
+        System.out.println(response.body());
         if (response.statusCode() != 200)
             throw new RuntimeException(response.body());
         ObjectMapper mapper = new ObjectMapper();
@@ -83,36 +101,32 @@ public class CreateExpense {
 
     private long convertDate(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        System.out.println(date);
         LocalDate localDate = LocalDate.parse(date, formatter);
-        long epoch = localDate.atStartOfDay(ZoneId.of("America/Sao_Paulo")).toInstant().toEpochMilli();
-        System.out.println(epoch);
-        return epoch;
+        return localDate.atStartOfDay(ZoneId.of("GMT")).toInstant().toEpochMilli();
     }
 
-    private boolean generateSP(String pgsCodSeq, ObjectNode data) throws IOException, InterruptedException {
+    private void generateSP(String pgsCodSeq, ObjectNode data) throws IOException, InterruptedException {
         HttpResponse<String> response = conexosApi.PostRequest("imp021/processoSp/btConfirmarGerarSp", "{\n" +
                 "    \"priCod\": " + data.get("priCod").asString() + ",\n" +
+                "    \"filCod\": 1," +
                 "    \"docVldTipo\": 9,\n" +
                 "    \"gcdCod\": 80,\n" +
                 "    \"frontModelName\": \"gerConfiguraDoc\",\n" +
                 "    \"pesCod\": " + data.get("COD_PESSOA_CEDENTE").asString() + ",\n" +
                 "    \"endCod\": " + data.get("endCod").asString() + ",\n" +
                 "    \"gcdDesNome\": \"DESPESAS RECIBO IMPORTAÇÃO - PRÓPRIO\",\n" +
-                "    \"docDtaEntrada\": " + Instant.now().toEpochMilli() + ",\n" +
+                "    \"docDtaEntrada\": " + LocalDate.now().atStartOfDay(ZoneId.of("GMT")).toInstant().toEpochMilli() + ",\n" +
                 "    \"dtaEmissao\": " + convertDate(data.get("EMISSAO").asString()) + ",\n" +
                 "    \"dtaValidade\": " + convertDate(data.get("VENCIMENTO").asString()) + ",\n" +
                 "    \"pdcDocFederal\": \"" + data.get("CNPJ").asString().replaceAll("[^a-zA-Z0-9]", "") + "\",\n" +
+                "    \"espNumero\": \"" + data.get("NUM_DOCUMENTO").asString() + "\",\n" +
                 "    \"items\": [\n" +
                 "        {\n" +
                 "            \"pgsCodSeq\": " + pgsCodSeq + "\n" +
                 "        }\n" +
                 "    ]\n" +
                 "}");
-        return true;
-    }
-
-    private boolean getAddressCode() {
-        return true;
+        if (response.statusCode() != 200) throw new RuntimeException(response.body());
+        System.out.println(response.body());
     }
 }
